5970cc8dba39126cce625b653758ed50bd75fe10
leoflotor <leonardoflotor@gmail.com>
2022-03-20 17:43:34 -0600

    euler project problem 001 rust solution refactorized

--- project_euler_rust/problem_001/src/lib.rs
+++ project_euler_rust/problem_001/src/lib.rs
@@ -1,14 +1,4 @@
-pub fn sum_of_multiples_below_number(number: i32) -> i32 {
-    let mut vec = Vec::new();
-    for n in 1..number {
-        if n % 5 == 0 || n % 3 == 0 {
-            vec.push(n);
-        }
-    }
-    vec.iter().sum()
+pub fn sum_of_multiples_below_number(number: i64) -> i64 {
+    let iter: i64 = (1..number).filter(|n| n % 5 == 0 || n % 3 == 0).sum();
+    iter
 }
-
-// fn main() {
-//     let sum = sum_of_multiples_below_number(10);
-//     println!("{}", sum)
-// }
--- project_euler_rust/problem_001/tests/problem_001.rs
+++ project_euler_rust/problem_001/tests/problem_001.rs
@@ -4,13 +4,3 @@ use problem_001::sum_of_multiples_below_number;
 fn test_multiples() {
     assert_eq!(sum_of_multiples_below_number(10), 23)
 }
-
-// #[cfg(test)]
-// mod tests {
-//     use problem_001::sum_of_multiples_below_number;
-
-//     #[test]
-//     fn test_multiples() {
-//         assert_eq!(sum_of_multiples_below_number(10), 23)
-//     }
-// }
